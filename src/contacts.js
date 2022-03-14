544bb60e49ca95b656fdfd1cca508f933a0b652d
vijay <vijaygohel5454@gmail.com>
2022-03-14 13:27:27 +0530

    complete

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Rock",
+    imgURL:
+      "https://static.wikia.nocookie.net/jumanji/images/f/f1/Dwayne_Johnson_2%2C_2013.jpg",
+    phone: "+123 456 789",
+    email: "dj@rock.com"
+  },
+  {
+    id: 2,
+    name: "Tom Cruise",
+    imgURL:
+      "https://vz.cnwimg.com/thumb-1200x/wp-content/uploads/2009/09/tc.jpg",
+    phone: "+987 654 321",
+    email: "tom@mail.com"
+  },
+  {
+    id: 3,
+    name: "Arnold",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg",
+    phone: "+918 372 574",
+    email: "arnold@gmail.com"
+  }
+];
+
+export default contacts;
